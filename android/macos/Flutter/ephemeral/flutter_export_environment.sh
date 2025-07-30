2a31a25858f5bb766a4dbd0f01723e2ba2c9f5cc
RajThummar0 <rajthummar2512@gmail.com>
2025-07-30 12:55:59 +0530

    Update flutter_export_environment.sh

--- android/macos/Flutter/ephemeral/flutter_export_environment.sh
+++ android/macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -5,6 +5,8 @@ export "FLUTTER_APPLICATION_PATH=C:\flutter project\dr_health\flutter_applicatio
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
+
+
 export "FLUTTER_BUILD_NUMBER=1"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
